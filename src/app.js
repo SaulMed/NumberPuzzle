49fbbd01b3a4dcbb5137abba111d46ef92b6fd70
SaulMed <elesedelsaul@gmail.com>
2024-10-18 19:35:56 -0600

    Se agrega tablero y funcion random

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-alert("Jelou")
+createNumbers = () => {
+    //create array
+    let array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    //aply random order
+    array.sort(() => Math.random() - .5);
+    console.log(array)
+}
+
+createNumbers();
