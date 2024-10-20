f3a26f0469bf379e6bf5164c90f057edf7692639
Dario105 <darioabednego@gmail.com>
2024-10-19 20:53:48 -0600

    agregando funcionalidad resetNumbers y asignando funcion getNeighbors

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
-//create array
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 0];
 
+/**
+ * @description apply random order to array
+ * @returns {void}
+ */
 createNumbers = () => {
-    //aply random order
     array.sort(() => Math.random() - .5);
-    // console.log(array)
 }
 
-//Create divs elements and add to parent element
+/**
+ * @description Create divs elements and add to parent element
+ * @returns {void}
+ */
 createDivs = () => {
-    const divs = document.getElementById("cuadro");
-    
+    const container = document.createElement("div");
+    container.id = "cuadro";
+    container.className = "cuadro";
+
     for(i = 0; i <= array.length -1; i++){
         let divElement = document.createElement("div");
+        divElement.className = "divs";
         if(array[i] == 0){
             divElement.textContent = null;
             divElement.setAttribute("id","vacio");
@@ -20,20 +27,22 @@ createDivs = () => {
         }else{
             divElement.textContent = array[i];
         }
-        
+        /*
         divElement.addEventListener("click",()=>{
             console.log("posicion: ", divElement.id);
         });
-        
-        // console.log(divElement);
-        divs.appendChild(divElement);
+        */
+        container.appendChild(divElement);
     }
+    document.body.appendChild(container);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    createNumbers();
-    createDivs();
 
+/**
+ * @description get the neighbors only up, down, right, left in the board
+ * @returns {void}
+ */
+getNeighbors = ()=>{
     let cells = document.querySelectorAll(".cuadro div");
 
     let vacio = document.querySelector("#vacio");
@@ -48,17 +57,36 @@ document.addEventListener("DOMContentLoaded", function () {
     ]
 
     indicesVecinos.filter(i => i !== null && i >= 0 && i < cells.length).forEach( i => {
-        console.log(i)
+        //console.log(i)
         let celda = cells[i];
-        console.log(celda)
+        //console.log(celda)
         celda.style.cursor = "pointer"
         celda.addEventListener('click', () => {
             alert("Vecino "+i)
         });
     });
+}
 
+/**
+ * @description this function resets the numbers
+ * remove the container and create divs elements with their neighbors
+ * @returns {void}
+ */
+resetNumbers = ()=>{
+    createNumbers();
+    let container = document.getElementById("cuadro");
+    container.remove();
+    createDivs();
+    getNeighbors();
+    
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+    createNumbers();
+    createDivs();
+    getNeighbors();
 
 });
+
 
 
